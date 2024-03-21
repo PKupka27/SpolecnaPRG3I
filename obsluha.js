fbbb5264be8c289dfe32e13ae8c4a049c52193e8
Marek Novotný <novotny.marek@odbornaskola.cz>
2024-03-21 14:20:06 +0100

    4.

--- obsluha.js
+++ obsluha.js
@@ -41,6 +41,11 @@ window.onload = function(){
 
     // vse pomoci cyklů, filtrů apod.
     // 4 Do firmy Meta vlozte zamestnance s pozicí Software a věkem do 50 let 
+
+    employees.filter(employee => employee.position === 'Software' && employee.age <= 50).forEach(employee => {
+      meta.addEmployee(employee);
+    });
+    
     // 5 Do Apple vlozte zamestnance s pozicí Software nebo Hardware
     // 6 do Microsoft vlozte pouze zamestnance s pozicí Marketing nebo zamestnance s mestem zacinajicím na San
     // 7 firmy vlozte do pole 
