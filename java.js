9a11086a436bf5bbb191b4b88db994e946af33a0
Josef Rajnis <jrajnis@seznam.cz>
2024-04-11 13:58:36 +0200

    3

--- java.js
+++ java.js
@@ -8,3 +8,37 @@ function stahniJSON(url, callback)
     xhr.send();
 }
 
+function nactiSeznam(url)
+{
+    let seznam_div = document.getElementById("poke-seznam");
+    seznam_div.innerHTML = "<ul id='poke-seznam-ul'></ul>";
+    let seznam_ul = document.getElementById("poke-seznam-ul");
+
+    stahniJSON(url, (data) => {
+        for (let pokemon of data.results)
+        {
+            let novaPolozka = document.createElement("li");
+            novaPolozka.innerText = pokemon.name;
+            seznam_ul.appendChild(novaPolozka);
+            let poke_url = pokemon.url;
+            novaPolozka.addEventListener("click", () => nactiPokemona(poke_url));
+        }
+    });
+}
+
+function nactiPokemona(url)
+{
+    let detaily_div = document.getElementById("poke-detaily");
+    stahniJSON(url, (data) => {
+        let html = `
+            <img src="${data.sprites.back_default}" />
+            <ul>
+                <li>Název: ${data.name}</li>
+                <li>Výška: ${data.height}</li>
+                <li>Váha: ${data.weight}</li>
+            </ul>
+        `;
+
+        detaily_div.innerHTML = html;
+    });
+}
