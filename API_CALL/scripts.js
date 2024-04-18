17a83b975152474ca4a974433dc980cb6627d0b1
test <test>
2024-04-18 14:36:36 +0200

    Nevim nefunguje to

--- API_CALL/scripts.js
+++ API_CALL/scripts.js
@@ -1,1 +1,24 @@
-let 
+window.onload = function(){
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    let btn = document.getElementById("sndCmd");
+
+    const options = {
+        method: 'GET'
+    };
+
+    btn.onclick = function() {
+        callApi(url, { options });
+    };
+    
+    async function callApi(callUrl, callOptions) {
+        try {
+            const response = await fetch(callUrl, callOptions);
+            const result = await response.text();
+            const message = JSON.parse(result)
+            document.getElementById('sndCmd').src = message.message;
+            return message;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
