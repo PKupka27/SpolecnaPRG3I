8519f91670a02a534a3d76e63e97c7780dcec448
test <test>
2024-05-16 13:25:57 +0200

    Přidán indexor předchozích souborů

--- DOG1/main.js
+++ DOG1/main.js
@@ -2,6 +2,8 @@ window.onload = function(){
     const url = 'https://dog.ceo/api/breeds/image/random';
     let btnnxt = document.getElementById("sndCmd");
     let btnbck = document.getElementById("zpetCmd");
+    let dogImageHistory = [];
+    let currentImageIndex = -1;
 
     const options = {
         method: 'GET'
@@ -20,6 +22,8 @@ window.onload = function(){
             const response = await fetch(callUrl, callOptions);
             const result = await response.text();
             const message = JSON.parse(result)
+            dogImageHistory.push(message.message);
+            currentImageIndex = dogImageHistory.length - 1;
             document.getElementById('dogImage').src = message.message;
             return message;
         } catch (error) {
@@ -27,15 +31,11 @@ window.onload = function(){
         }
     }
 
-    async function callApiPrev(callUrl, callOptions) {
-        try {
-            const response = await fetch(callUrl, callOptions);
-            const result = await response.text();
-            const message = JSON.parse(result)
-            document.getElementById('dogImage').src = message.message;
-            return message;
-        } catch (error) {
-            console.error(error);
+    function callApiPrev(callUrl, callOptions) {
+        if (previousDogImage !== null) {
+            document.getElementById('dogImage').src = previousDogImage;
+        } else {
+            console.log('No previous image available.');
         }
     }
 }
