1de92ede38e086d4807e5aa2e172cce91ce0e47b
test <test>
2024-05-16 13:48:02 +0200

    Bylo potřeba upravit bloky volající funkce.

--- DOG2/main.js
+++ DOG2/main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
-    const url = 'https://dog.ceo/api/breeds/image/random';
     let btnnxt = document.getElementById("sndCmd");
     let btnbck = document.getElementById("zpetCmd");
+    let btnMulti = document.getElementById("sendFragmentCmd");
     let dogImageHistory = [];
     let currentImageIndex = -1;
 
@@ -10,35 +10,29 @@ window.onload = function(){
     };
 
     btnnxt.onclick = function() {
-        callApi(url, { options });
+        if (currentImageIndex < dogImageHistory.length - 1) {
+            currentImageIndex++;
+            document.getElementById('dogImage').src = dogImageHistory[currentImageIndex];
+        }
     };
 
-    btnbck.onclick = function() {
-        callApiPrev(url, { options });
+    btnMulti.onclick = function() {
+        const count = document.getElementById("pesindex").value;
+        if (count > 0) {
+            callMultiApi(count);
+        }
     };
-    
-    async function callApi(callUrl, callOptions) {
+
+    async function callMultiApi(count) {
         try {
-            const response = await fetch(callUrl, callOptions);
-            const result = await response.text();
-            const message = JSON.parse(result)
-            dogImageHistory.push(message.message);
-            currentImageIndex = dogImageHistory.length - 1;
-            document.getElementById('dogImage').src = message.message;
-            return message;
+            const url = `https://dog.ceo/api/breeds/image/random/${count}`;
+            const response = await fetch(url, options);
+            const result = await response.json();
+            dogImageHistory = result.message;
+            currentImageIndex = 0;
+            document.getElementById('dogImage').src = dogImageHistory[currentImageIndex];
         } catch (error) {
             console.error(error);
-        }
-    }
-
-    // Pokud se vrátíme zpět o několik obrázků, tak až znovu stiskneme Next, tak už se načtou ale nové obrázky!
-
-    function callApiPrev() {
-        if (currentImageIndex > 0) {
-            currentImageIndex--;
-            document.getElementById('dogImage').src = dogImageHistory[currentImageIndex];
-        } else {
-            console.log('Žádný předchozí img.');
         }
     }
 }
