bf2ee333bca81699c12763806358b952a69760ed
alsonix <jakubvojtisek0@gmail.com>
2024-06-04 19:52:22 +0200

    Přidány konstanty pro JS

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Public Konstanty START //
+    const addTaskButton = document.getElementById('add-task');
+    const newTaskInput = document.getElementById('new-task');
+    const taskList = document.getElementById('task-list');
+    const currentTimeDisplay = document.getElementById('current-time');
+    const machineIp = document.getElementById('localmachine-ip');
+    // Public Konstanty END //
+});
